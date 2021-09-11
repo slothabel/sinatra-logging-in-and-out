f1840a0c8760b7ccf6b608b7436eae2640c2ce3a
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-11 16:30:58 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
+require 'pry'
 require_relative '../../config/environment'
+require_relative '../../app/helpers/helpers.rb'
 class ApplicationController < Sinatra::Base
+include Helpers
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
@@ -11,12 +14,24 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    erb :error
   end
 
   get '/account' do
-
+    # binding.pry
+    binding.pry
+    @user = User.find_by(id: session[:user_id])
+    if !@user.is_logged_in? 
+      erb :error
+    end
+    erb :account
   end
+
 
   get '/logout' do
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,18 @@
-class Helpers
+require 'pry'
+module Helpers
+
+def self.current_user(session)
+    @user = User.find_by(id: session[:user_id])
+    @user
+end
+
+def self.is_logged_in?(session)
+    @user = User.find_by(id: session[:user_id])
+    if !!session.include?(@user)
+        false
+    else
+        true
+    end
+end
+
 end
